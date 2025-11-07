e21316851af9701ba69fd02a3cf55f2495cc4244
Thays Peixoto <thabysilva12@gmail.com>
2025-11-07 17:45:16 -0300

    componente card que recebe props agregando as devidas informações

---
+++ src/components/card/index.jsx
@@ -0,0 +1,15 @@
+import "./style.css";
+import React from "react";
+
+export function Card({ title, description, image }) {
+  return (
+    <article className="card-container" aria-label={title}>
+      <img src={image} alt={title} className="card-image" />
+
+      <div className="card__body">
+        <h3 className="card__title">{title}</h3>
+        <p className="card__description">{description}</p>
+      </div>
+    </article>
+  );
+}
