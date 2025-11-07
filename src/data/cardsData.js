ef7f23a236e92cc6d888d03ff212566966f4ae70
Thays Peixoto <thabysilva12@gmail.com>
2025-11-07 17:41:53 -0300

    desenvolvendo objetos para itens card

---
+++ src/data/cardsData.js
@@ -0,0 +1,23 @@
+export const cardsData = [
+  {
+    id: "card-1",
+    title: "Meu primeiro card",
+    description: "Descrição curta do primeiro card.",
+    image:
+      "https://plus.unsplash.com/premium_photo-1664117436431-aaa0d75814fe?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1974s",
+  },
+  {
+    id: "card-2",
+    title: "Segundo card",
+    description: "Descrição do segundo card.",
+    image:
+      "https://images.unsplash.com/photo-1720572181870-9fbd04f3591d?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=387",
+  },
+  {
+    id: "card-3",
+    title: "Terceiro card",
+    description: "Descrição do terceiro card.",
+    image:
+      "https://images.unsplash.com/photo-1573548500833-02035bc24fb6?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGFwZWwlMjBkZSUyMHBhcmVkZSUyMHBhcmElMjBjZWx1bGFyJTIwNGt8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=600",
+  },
+];
